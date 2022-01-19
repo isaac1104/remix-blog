42a209fd99b9433e44c0da11322cff1d9e155a68
Isaac Kwon <isaac-kwon@hotmail.com>
2022-01-19 17:14:52 -0600

    Create form wip

--- app/routes/posts/index.jsx
+++ app/routes/posts/index.jsx
@@ -1,7 +1,36 @@
+import { Link, useLoaderData } from 'remix';
+
+export const loader = () => {
+  const data = {
+    posts: [
+      { id: 1, title: 'Post 1', body: 'This is post 1' },
+      { id: 2, title: 'Post 2', body: 'This is post 2' },
+      { id: 3, title: 'Post 3', body: 'This is post 3' },
+    ],
+  };
+  return data;
+};
+
 export default function PostItems() {
+  const { posts } = useLoaderData();
+
   return (
-    <div>
-      <h1>Post Items</h1>
-    </div>
+    <>
+      <div className='page-header'>
+        <h1>Posts</h1>
+        <Link to='/posts/new' className='btn'>
+          New Post
+        </Link>
+      </div>
+      <ul className='posts-list'>
+        {posts.map(({ id, title, body }) => (
+          <li key={id}>
+            <Link to={id}>
+              <h3>{title}</h3>
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </>
   );
 }

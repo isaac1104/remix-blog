8816d0ed93a49610d78bb136393ee327f3a3e02d
isaac1104 <isaac-kwon@hotmail.com>
2022-01-19 20:22:25 -0600

    Logout user

--- app/root.jsx
+++ app/root.jsx
@@ -1,5 +1,12 @@
-import { Outlet, LiveReload, Link, Links, Meta } from 'remix';
+import { Outlet, LiveReload, Link, Links, Meta, useLoaderData } from 'remix';
+import { getUser } from '~/utils/session.server';
 import globalStyles from '~/styles/global.css';
+
+export const loader = async ({ request }) => {
+  const user = await getUser(request);
+  const data = { user };
+  return data;
+};
 
 export default function App() {
   return (
@@ -34,6 +41,8 @@ function Document({ children, title }) {
 }
 
 function Layout({ children }) {
+  const { user } = useLoaderData();
+
   return (
     <>
       <nav className='navbar'>
@@ -44,9 +53,19 @@ function Layout({ children }) {
           <li>
             <Link to='/posts'>Posts</Link>
           </li>
-          <li>
-            <Link to='/auth/login'>Login</Link>
-          </li>
+          {user ? (
+            <li>
+              <form action='/auth/logout' method='POST'>
+                <button className='btn' type='submit'>
+                  Logout {user.username}
+                </button>
+              </form>
+            </li>
+          ) : (
+            <li>
+              <Link to='/auth/login'>Login</Link>
+            </li>
+          )}
         </ul>
       </nav>
       <div className='container'>{children}</div>
--- app/utils/session.server.ts
+++ app/utils/session.server.ts
@@ -45,3 +45,34 @@ export async function createUserSession(userId: string, redirectTo: string) {
     },
   });
 }
+
+export function getUserSession(request: Request) {
+  return storage.getSession(request.headers.get('Cookie'));
+}
+
+export async function getUser(request: Request) {
+  const session = await getUserSession(request);
+  const userId = session.get('userId');
+
+  if (!userId || typeof userId !== 'string') return null;
+
+  try {
+    const user = await db.user.findUnique({
+      where: {
+        id: userId,
+      },
+    });
+    return user;
+  } catch (error) {
+    return null;
+  }
+}
+
+export async function logout(request: Request) {
+  const session = await storage.getSession(request.headers.get('Cookie'));
+  return redirect('/auth/logout', {
+    headers: {
+      'Set-Cookie': await storage.destroySession(session),
+    },
+  });
+}

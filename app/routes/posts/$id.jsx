84749285aff44a965e707961132ead18c2c7bc22
isaac1104 <isaac-kwon@hotmail.com>
2022-01-19 19:10:30 -0600

    Create, read & delete post

--- app/routes/posts/$id.jsx
+++ app/routes/posts/$id.jsx
@@ -1,11 +1,54 @@
-import { useParams } from 'remix';
+import { Link, useLoaderData, redirect } from 'remix';
+import { db } from '~/utils/db.server';
+
+export const loader = async ({ params }) => {
+  const post = await db.post.findUnique({
+    where: { id: params.id },
+  });
+
+  if (!post) throw new Error('Post not found');
+
+  return {
+    post,
+  };
+};
+
+export const action = async ({ request, params }) => {
+  const form = await request.formData();
+
+  if (form.get('_method') === 'delete') {
+    const post = await db.post.findUnique({
+      where: { id: params.id },
+    });
+
+    if (!post) throw new Error('Post not found');
+
+    await db.post.delete({ where: { id: params.id } });
+    return redirect('/posts');
+  }
+};
 
 export default function Post() {
-  const params = useParams();
+  const {
+    post: { title, body },
+  } = useLoaderData();
 
   return (
     <div>
-      <h1>{params.id}</h1>
+      <div className='page-header'>
+        <h1>{title}</h1>
+        <Link to='/posts' className='btn btn-reverse'>
+          Back
+        </Link>
+      </div>
+      <div className='page-content'>{body}</div>
+      <div className='page-footer'>
+        {/* Let the server know that this is a delete request */}
+        <form method='POST'>
+          <input type='hidden' name='_method' value='delete' />
+          <button className='btn btn-delete'>Delete</button>
+        </form>
+      </div>
     </div>
   );
 }
--- app/routes/posts/new.jsx
+++ app/routes/posts/new.jsx
@@ -1,4 +1,5 @@
 import { Link, redirect } from 'remix';
+import { db } from '~/utils/db.server';
 
 export const action = async ({ request }) => {
   const form = await request.formData();
@@ -7,7 +8,9 @@ export const action = async ({ request }) => {
 
   const fields = { title, body };
 
-  // return redirect('/posts');
+  const { id } = await db.post.create({ data: fields })
+
+  return redirect(`/posts/${id}`);
 };
 
 export default function NewPost() {
